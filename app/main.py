bca4cb236399b188351fd21a2984b4b2809369e3
Gabriel <gferrazsiqueira05@gmail.com>
2026-03-27 23:09:57 -0300

    Result

--- app/main.py
+++ app/main.py
@@ -9,14 +9,13 @@ class Person:
 
 def create_person_list(people: list) -> list:
     Person.people.clear()
-    for peo in people:
-        Person(peo["name"], peo["age"])
-    for peo in people:
-        person_inst = Person.people[peo["name"]]
-        wife_name = peo.get("wife")
+    [Person(person_data["name"], person_data["age"]) for person_data in people]
+    for person_data in people:
+        person_inst = Person.people[person_data["name"]]
+        wife_name = person_data.get("wife")
         if wife_name:
             person_inst.wife = Person.people[wife_name]
-        husband_name = peo.get("husband")
+        husband_name = person_data.get("husband")
         if husband_name:
             person_inst.husband = Person.people[husband_name]
-    return [Person.people[peo["name"]] for peo in people]
+    return [Person.people[person_data["name"]] for person_data in people]
